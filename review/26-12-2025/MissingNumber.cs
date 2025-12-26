e6741d7f9a2e38be97a5ea7e04ee4fa66f2db575
devanshchaudhary102003 <devanshchaudhary102003@gmail.com>
2025-12-26 14:27:50 +0530

    [Devansh Singh] Add.review solution 26Dev25

---
+++ review/26-12-2025/MissingNumber.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Numerics;
+class MissingNumber
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Length");
+        
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        int max = int.MinValue;
+        int sum = 0;
+        int result = 0;
+        for(int i = 0; i < n; i++)
+        {
+           max = Math.Max(arr[i],max);
+           int ans = ((max)*(max+1))/2;
+           sum += arr[i];
+
+            result = ans - sum;
+        }
+        Console.WriteLine("Missing Number is :"+result);
+    }
+}
