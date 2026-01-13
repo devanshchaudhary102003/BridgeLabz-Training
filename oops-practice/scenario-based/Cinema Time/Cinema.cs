2eb3de9e718d6a07495d19898a235db6a6eb025b
devanshchaudhary102003 <devanshchaudhary102003@gmail.com>
2026-01-13 16:32:11 +0530

    [Devansh Singh] Add.Cinema Time

---
+++ oops-practice/scenario-based/Cinema Time/Cinema.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTime
+{
+    internal class Cinema
+    {
+        private string Title;
+        private string Time;
+
+        public string title
+        {
+            get
+            {
+                return Title;
+            }
+            set
+            {
+                Title = value;
+            }
+        }
+
+        public string time
+        {
+            get
+            {
+                return Time;
+            }
+            set
+            {
+                Time = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Movie Title: " + title + " Movie Timing: " + time;
+        }
+    }
+}
---
+++ oops-practice/scenario-based/Cinema Time/CinemaMenu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTime
+{
+    internal class CinemaMenu
+    {
+        CinemaUtilityImpl CinemaUtility = new CinemaUtilityImpl();
+        public void Menu()
+        {
+            int choice;
+
+            do
+            {
+                Console.WriteLine("\n🎬 CinemaTime – Movie Schedule Manager");
+                Console.WriteLine("1. Add Movie");
+                Console.WriteLine("2. Search Movie");
+                Console.WriteLine("3. Display All Movies");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter your choice: ");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter Movie Title: ");
+                        string title = Console.ReadLine();
+
+                        Console.Write("Enter Show Time: ");
+                        string time = Console.ReadLine();
+
+                        CinemaUtility.AddMovie(title, time);
+                        break;
+
+                    case 2:
+                        Console.Write("Enter keyword to search: ");
+                        string keyword = Console.ReadLine();
+                        CinemaUtility.SearchMovie(keyword);
+                        break;
+
+                    case 3:
+                        CinemaUtility.DisplayAllMovies();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Exiting application...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+
+            } while (choice != 4);
+        }
+    }
+}
