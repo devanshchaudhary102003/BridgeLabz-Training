6e54a0f3c3acc3bcf550a04411c2dec16e4a1ac1
devanshchaudhary102003 <devanshchaudhary102003@gmail.com>
2026-01-14 15:29:40 +0530

    [Devansh Singh] Refactor.Add new Contact to Address Book

---
+++ oops-practice/scenario-based/Address Book/AddressBookMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_System
+{
+    internal class AddressBookMenu
+    {
+        IAddressBook Contact = new AddressBookUtilityImpl();
+
+        public void Menu()
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("\n===== ADDRESS BOOK MENU =====");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice: ");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Contact.AddContact();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+
+            } while (choice != 0);
+        }
+    }
+}
---
+++ oops-practice/scenario-based/Address Book/AddressBookUtilityImpl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_System
+{
+    internal class AddressBookUtilityImpl:IAddressBook
+    {
+        private AddressBook[] addressBooks = new AddressBook[10];
+
+        private int count = 0;
+
+        public void AddContact()
+        {
+            if(count == addressBooks.Length)
+            {
+                Console.WriteLine("Address Book is full. Cannot and More Contacts");
+                return;
+            }
+
+            AddressBook contact = new AddressBook();
+
+            Console.WriteLine("Enter First Name: ");
+            contact.firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            contact.lastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            contact.address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            contact.city = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            contact.state = Console.ReadLine();
+
+            Console.Write("Enter Zip: ");
+            contact.zip = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            contact.phonenumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            contact.email = Console.ReadLine();
+
+            addressBooks[count] = contact;
+            count++;
+
+            Console.WriteLine("\nContact added Successfully!");
+        }
+
+    }
+}
